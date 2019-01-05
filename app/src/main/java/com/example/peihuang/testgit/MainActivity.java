2db72e693f4ae9351d624c15a2caffc03f1102f1
Pei Huang <peihuang@google.com>
2019-01-05 23:06:48 +0800

    Merge branch 'macTestBranch4'

--- app/src/main/java/com/example/peihuang/testgit/MainActivity.java
+++ app/src/main/java/com/example/peihuang/testgit/MainActivity.java
@@ -112,6 +112,10 @@ public class MainActivity extends AppCompatActivity {
     {
 
     }
+    public void macBranch4()
+    {
+
+    }
 
     /**
      * A placeholder fragment containing a simple view.
